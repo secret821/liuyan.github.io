967a357d12d3b1e5755ac6d194a14b96c07d77eb
“liuyan” <826148658@qq.com>
2021-11-08 17:07:46 +0800

    feat:添加Travis CI

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git config --local user.name "刘妍"
+git config --local user.email "826148658@qq.com"
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 如果使用 travis 持续集成
+git push -f https://github.com/secret821/liuyan.github.io.git
+
+cd -
